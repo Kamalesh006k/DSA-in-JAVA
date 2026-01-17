473da33a363c43c8040caa5ee4e5e5ce7f0e5781
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-01-17 15:23:11 +0530

    Prefix max array added

--- Arrays/Max_Consecutive_Ones/MaxConsecutiveOnes.java
+++ Arrays/Max_Consecutive_Ones/MaxConsecutiveOnes.java
@@ -16,8 +16,10 @@ public class MaxConsecutiveOnes {
     }
 	public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements:");
         int n = sc.nextInt();
         int[] arr =  new int[n];
+        System.out.print("Enter the Array elements....");
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
