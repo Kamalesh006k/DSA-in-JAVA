7161a79c627322b26d06eaac7ccbb4cf02de85e5
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-01-27 21:32:40 +0530

    add left commits

--- Trees/Max_Segment_Tree/MaxSegTree.java
+++ Trees/Max_Segment_Tree/MaxSegTree.java
@@ -1,8 +1,8 @@
 import java.util.*;
-public class SegTree {
+public class MaxSegTree {
     int[] SegTree;
 
-    SegTree(int n){
+    MaxSegTree(int n){
         SegTree = new int[n*2-1];
     }
 
@@ -14,18 +14,18 @@ public class SegTree {
         int mid = low +(high - low)/2;
         build(arr, seg,2*i+1, low, mid);
         build(arr, seg,2*i+2,mid+1, high);
-        seg[i] = Math.min(seg[2*i+1],seg[2*i+2]);
+        seg[i] = Math.max(seg[2*i+1],seg[2*i+2]);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of elements to insert into the arrary: ");
+        System.out.print("Enter the number of elements to build into the arrary: ");
         int n = sc.nextInt();
-        System.out.print("Enter the array elements.....");
+        System.out.println("Enter the array elements.....");
         int[] arr = new int[n];
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        SegTree seg1 = new SegTree(n);
+        MaxSegTree seg1 = new MaxSegTree(n);
         seg1.build(arr, seg1.SegTree, 0, 0, n-1);
         System.out.println("The minimum element in an array: "+seg1.SegTree[0]);
         System.out.print("Enter the number of elements to insert into the arrary: ");
@@ -35,7 +35,7 @@ public class SegTree {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        SegTree seg2 = new SegTree(k);
+        MaxSegTree seg2 = new MaxSegTree(k);
         seg1.build(arr, seg2.SegTree, 0, 0, n-1);
         System.out.println("The minimum element in an array: "+seg2.SegTree[0]);
         sc.close();
