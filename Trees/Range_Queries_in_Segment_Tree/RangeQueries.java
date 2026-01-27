aaf3a5bf0888ed8d0c24fb869e08ab87b3c406a5
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-01-27 12:37:25 +0530

    Range queries added

--- Trees/Range_Queries_in_Segment_Tree/RangeQueries.java
+++ Trees/Range_Queries_in_Segment_Tree/RangeQueries.java
@@ -1,47 +1,51 @@
 import java.util.*;
-
 public class RangeQueries {
+    int[] SegTree;
 
-    static void build(int[] arr, int[] seg, int i, int left, int right) {
-        if (left == right) {
-            seg[i] = arr[left];
+    RangeQueries(int n){
+        SegTree = new int[n*2-1];
+    }
+
+    void build(int[] arr, int[] seg, int i,int low, int high){
+        if(low == high){
+            seg[i] = arr[low];
             return;
         }
-
-        int mid = left + (right - left) / 2;
-        build(arr, seg, 2 * i + 1, left, mid);
-        build(arr, seg, 2 * i + 2, mid + 1, right);
-        seg[i] = Math.min(seg[2 * i + 1], seg[2 * i + 2]);
+        int mid = low +(high - low)/2;
+        build(arr, seg,2*i+1, low, mid);
+        build(arr, seg,2*i+2,mid+1, high);
+        seg[i] = Math.min(seg[2*i+1],seg[2*i+2]);
     }
 
-    static int findMin(int[] seg, int i, int left, int right, int l, int r) {
-        if (left >= l && right <= r)
+    int rangeQuery(int[] arr, int[] seg, int i, int low, int high, int l, int r){
+        if(low>=l && high<=r){
             return seg[i];
-
-        if (right < l || left > r)
+        }
+        if(high<l || low>r){
             return Integer.MAX_VALUE;
-
-        int mid = left + (right - left) / 2;
-        int vleft = findMin(seg, 2 * i + 1, left, mid, l, r);
-        int vright = findMin(seg, 2 * i + 2, mid + 1, right, l, r);
-
-        return Math.min(vleft, vright);
+        }
+        int mid = low+(high-low)/2;
+        int left = rangeQuery(arr, seg, 2*i+1, low, mid, l, r);
+        int right = rangeQuery(arr, seg, 2*i+2, mid+1, high, l, r);
+        return Math.min(left,right);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of elements to insert into the array: ");
+        System.out.print("Enter the number of elements to insert into the arrary: ");
         int n = sc.nextInt();
+        System.out.println("Enter the array elements.....");
         int[] arr = new int[n];
-        System.out.println("Enter the elements....");
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
-        int[] seg = new int[4 * n];
-        build(arr, seg, 0, 0, n - 1);
-        System.out.print("Enter the value of l: ");
+        }
+        RangeQueries rq = new RangeQueries(n);
+        rq.build(arr, rq.SegTree, 0, 0, n-1);
+        System.out.println("Enter the left range value: ");
         int l = sc.nextInt();
-        System.out.print("Enter the value of r: ");
+        System.out.println("Enter the right range value: ");
         int r = sc.nextInt();
-        System.out.println("The minimum element within the given range is "+findMin(seg, 0, 0, n - 1, l, r));
+        int min = rq.rangeQuery(arr,rq.SegTree,0,0,n-1,l,r);
+        System.out.println("The minimum element in the given range: "+min);
+        sc.close();
     }
 }
