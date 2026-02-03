49ca3502e06e6d84ebcb7218c93b6310ebbc35af
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-02-03 10:44:54 +0530

    xo game optimized

--- Graphs/XO_Game/Game.java
+++ Graphs/XO_Game/Game.java
@@ -41,17 +41,20 @@ class Game {
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(i==0 || i==n-1 || j==0 || j==m-1){
-                    game(i,j,true,arr,visited);
+                    if(arr[i][j] == 'O'){
+                        game(i,j,true,arr,visited);
+                    }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i!=0 && i!=n-1 && j!=0 && j!=m-1){
-                    game(i,j,false,arr,visited);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 'O' && !visited[i][j]) {
+                    arr[i][j] = 'X';
                 }
             }
         }
+
         System.out.println();
 
         for(char[] s:arr){
