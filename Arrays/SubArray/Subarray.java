e1d47877dfdca3a3f01f2f462739ba9958bc52e3
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-02-21 23:32:55 +0530

    subarray recursion approach

--- Arrays/SubArray/Subarray.java
+++ Arrays/SubArray/Subarray.java
@@ -1,7 +1,23 @@
-import java.util.Scanner;
-
-import java.util.;
+import java.util.*;
 public class Subarray {
+
+    static void subArray(int[] arr,int start, int end , List<Integer> list, List<List<Integer>> ans){
+        if(start == arr.length ){
+            return;
+        }
+        if(end == arr.length){
+            list.clear();
+            subArray(arr, start+1, start+1,list,ans);
+        }else{
+            list.clear();
+            for(int i=start; i<=end; i++){
+                list.add(arr[i]);
+            }
+            ans.add(new ArrayList<>(list));
+            subArray(arr, start, end+1,list, ans);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
@@ -9,7 +25,9 @@ public class Subarray {
         for(int i=0; i<n;  i++){
             arr[i] = sc.nextInt();
         }
-
-        
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        subArray(arr, 0, 0,list, ans);
+        System.out.println(ans);
     }
 }
