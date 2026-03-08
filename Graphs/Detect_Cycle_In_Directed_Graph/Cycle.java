71e2f05139a15e7c243fd6daa62f5324cd26abca
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-03-08 19:08:24 +0530

    Detect Cycle in a Directed Graph

---
+++ Graphs/Detect_Cycle_In_Directed_Graph/Cycle.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Cycle {
+
+    static boolean detectCycle(int node, List<List<Integer>> adj, int[] vis, int[] path){
+        vis[node] = 1;
+        path[node] = 1;
+        if(!adj.get(node).isEmpty()){
+            for(int s: adj.get(node)){
+                if(vis[s]==0){
+                    if(detectCycle(s, adj, vis, path)) return true;
+                }else{
+                    if(path[s] == 1) return true;
+                }
+            }
+        }
+        path[node] = 0;
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<m; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adj.get(u).add(v);
+        }
+
+        int[] vis = new int[n];
+        int[] path = new int[n];
+
+        for (int i = 0; i<n; i++) {
+            if(vis[i]==0){
+                if (detectCycle(i, adj, vis, path)) {
+                    System.out.println("True");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("False");
+    }
+}
