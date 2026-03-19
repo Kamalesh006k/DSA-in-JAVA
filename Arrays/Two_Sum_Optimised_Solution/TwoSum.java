1170033cbf4ff76f6eb3f0b0336ec50dc4aa4624
Kamaleshrajan S <kamaleshrajan006@gmail.com>
2026-03-19 14:58:42 +0530

    Two sum opt sol added

---
+++ Arrays/Two_Sum_Optimised_Solution/TwoSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class TwoSum {
+
+    static int[] twoSum(int[] arr, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            int temp = target - arr[i];
+            if(map.containsKey(temp)){
+                return new int[]{map.get(temp),i};
+            }else{
+                map.put(arr[i], i);
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] result = twoSum(arr, target);
+        for(int s: result){
+            System.out.println(s+" ");
+        }
+    }
+}
